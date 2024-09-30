dc9ef5f71cf640fc4d52b62ea9fa7d4e45d7b494
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-09-30 14:44:35 +0300

    blocked scope of defining variables

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,9 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const innerTask = true;
-    const innerTask2 = false;
-    return [innerTask, innerTask2];
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
