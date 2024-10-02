40bd602c5b11a9a481f3e3a4c28f5a309ac161ab
dominic6809 <muuomusyoki018@gmail.com>
2024-10-02 09:17:05 +0000

    Handle multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,15 @@
+/* eslint-disable import/extensions */
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+/* eslint-enable import/extensions */
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpPromise, uploadPhotoPromise])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
+}
