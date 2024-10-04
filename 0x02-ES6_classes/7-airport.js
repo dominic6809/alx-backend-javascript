3288491f9b7cb97ab0cce7e546e34d97675b7694
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-10-04 04:08:48 +0300

    Update 7-airport.js

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -3,37 +3,27 @@
 
 export default class Airport {
   constructor(name, code) {
-    this._name = this._validateName(name);
-    this._code = this._validateCode(code);
+    this._name = name;
+    this._code = code;
   }
 
-  // Getter for name
   get name() {
     return this._name;
   }
 
-  // Getter for code
+  set name(value) {
+    this._name = value;
+  }
+
   get code() {
     return this._code;
   }
 
-  // Override toString to return the airport code
+  set code(value) {
+    this._code = value;
+  }
+
   toString() {
-    return this._code;
-  }
-
-  // Validation methods
-  _validateName(name) {
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    return name;
-  }
-
-  _validateCode(code) {
-    if (typeof code !== 'string') {
-      throw new TypeError('Code must be a string');
-    }
-    return code;
+    return `[object ${this.code}]`;
   }
 }
