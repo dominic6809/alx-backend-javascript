f145cc1f97db2d4492d9d100c2dd20bd311f703e
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-10-04 04:07:23 +0300

    Update 100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,25 +1,15 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable no-unused-vars */
-
-import Car from './10-car.js';
-
-// Define private symbols for EVCar attributes
-const _range = Symbol('range');
+/* eslint-disable class-methods-use-this */
+import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
-    // Call the parent constructor
     super(brand, motor, color);
-    this[_range] = range; // Using Symbol to create a private attribute for range
+    this._range = range;
   }
 
-  // Getter for the range attribute
-  get range() {
-    return this[_range];
-  }
-
-  // Override the cloneCar method to return a Car instance
   cloneCar() {
-    return new Car(this.brand, this.motor, this.color);
+    return new Car();
   }
 }
