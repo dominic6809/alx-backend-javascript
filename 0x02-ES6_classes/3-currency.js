426d099b405e925e2f5e19604b8d54b1ed998ff8
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-10-04 03:40:14 +0300

    Update 3-currency.js

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,44 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = this._validateCode(code);
+    this._name = this._validateName(name);
+  }
+
+  // Getter and Setter for code.
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    this._code = this._validateCode(newCode);
+  }
+
+  // Getter and Setter for name
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = this._validateName(newName);
+  }
+
+  // Method to display the currency in "name (code)" format
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+
+  // Validation methods
+  _validateCode(code) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    return code;
+  }
+
+  _validateName(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    return name;
+  }
+}
