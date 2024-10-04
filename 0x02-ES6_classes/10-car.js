c9bdc29f10fe89a1b19f5254c13a2a61b18fd9d9
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-10-04 04:03:30 +0300

    Update 10-car.js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,33 +1,14 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable no-unused-vars */
 
-// Define private symbols for attributes
-const _brand = Symbol('brand');
-const _motor = Symbol('motor');
-const _color = Symbol('color');
-
 export default class Car {
   constructor(brand, motor, color) {
-    this[_brand] = brand;  // Using Symbol to create a private attribute
-    this[_motor] = motor;  // Using Symbol to create a private attribute
-    this[_color] = color;
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
-  // Getters for each attribute
-  get brand() {
-    return this[_brand];
-  }
-
-  get motor() {
-    return this[_motor];
-  }
-
-  get color() {
-    return this[_color];
-  }
-
-  // Method to clone the car object
   cloneCar() {
-    return new Car(this.brand, this.motor, this.color);
+    return new this.constructor(this._brand, this._motor, this._color);
   }
 }
