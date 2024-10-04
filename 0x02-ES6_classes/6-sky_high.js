2dbedceecd3608fe8b59c5ebfdc87c8f4d541687
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-10-04 03:53:53 +0300

    Update 6-sky_high.js

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,28 +1,23 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable no-unused-vars */
-import Building from './5-building.js';
+
+import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
-    super(sqft); // Call the parent class constructor with sqft
-    this._floors = this._validateFloors(floors); // Validate and set floors
+    super(sqft);
+    this._floors = floors;
   }
 
-  // Getter for floors
   get floors() {
     return this._floors;
   }
 
-  // Override evacuationWarningMessage method
-  evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
+  set floors(value) {
+    this._floors = value;
   }
 
-  // Validation method for floors
-  _validateFloors(floors) {
-    if (typeof floors !== 'number') {
-      throw new TypeError('Floors must be a number');
-    }
-    return floors;
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
