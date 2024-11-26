6bc9d5c2a9f1d5369df93cd06ae5824eaabfe764
Dominic Muuo <124082859+dominic6809@users.noreply.github.com>
2024-11-26 10:29:49 +0300

    Update StudentsController.js

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -3,55 +3,76 @@ import readDatabase from '../utils';
 /**
  * StudentsController handles the student-related routes.
  */
-export default class StudentsController {
-  /**
+  
+  const VALID_MAJORS = ['CS', 'SWE'];
+
+/**
    * Renders all students sorted by their major.
    *
    * @param {Object} req - The request object.
    * @param {Object} res - The response object.
    * @returns {void}
    */
-  static async getAllStudents(req, res) {
-    try {
-      const fields = await readDatabase(process.argv[2]);
-      res.status(200).write('This is the list of our students\n');
+class StudentsController {
+  static getAllStudents(request, response) {
+    const dataPath = process.argv.length > 2 ? process.argv[2] : '';
 
-      // Sort the fields alphabetically
-      Object.keys(fields)
-        .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))
-        .forEach((field) => {
-          const names = fields[field].join(', ');
-          res.write(`Number of students in ${field}: ${fields[field].length}. List: ${names}\n`);
-        });
+    readDatabase(dataPath)
+      .then((studentGroups) => {
+        const responseParts = ['This is the list of our students'];
+        // A comparison function for ordering a list of strings in ascending
+        // order by alphabetic order and case insensitive
+        const cmpFxn = (a, b) => {
+          if (a[0].toLowerCase() < b[0].toLowerCase()) {
+            return -1;
+          }
+          if (a[0].toLowerCase() > b[0].toLowerCase()) {
+            return 1;
+          }
+          return 0;
+        };
 
-      res.end();
-    } catch (error) {
-      res.status(500).send('Cannot load the database');
-    }
+        for (const [field, group] of Object.entries(studentGroups).sort(cmpFxn)) {
+          responseParts.push([
+            `Number of students in ${field}: ${group.length}.`,
+            'List:',
+            group.map((student) => student.firstname).join(', '),
+          ].join(' '));
+        }
+        response.status(200).send(responseParts.join('\n'));
+      })
+      .catch((err) => {
+        response
+          .status(500)
+          .send(err instanceof Error ? err.message : err.toString());
+      });
   }
 
-  /**
-   * Renders students based on the specified major.
-   *
-   * @param {Object} req - The request object.
-   * @param {Object} res - The response object.
-   * @returns {void}
-   */
-  static async getAllStudentsByMajor(req, res) {
-    const { major } = req.params;
+  static getAllStudentsByMajor(request, response) {
+    const dataPath = process.argv.length > 2 ? process.argv[2] : '';
+    const { major } = request.params;
 
-    if (major !== 'CS' && major !== 'SWE') {
-      res.status(500).send('Major parameter must be CS or SWE');
+    if (!VALID_MAJORS.includes(major)) {
+      response.status(500).send('Major parameter must be CS or SWE');
       return;
     }
+    readDatabase(dataPath)
+      .then((studentGroups) => {
+        let responseText = '';
 
-    try {
-      const fields = await readDatabase(process.argv[2]);
-      const names = fields[major] ? fields[major].join(', ') : '';
-      res.status(200).send(`List: ${names}`);
-    } catch (error) {
-      // Respond with an error if the database can't be loaded
-      res.status(500).send('Cannot load the database');
-    }
+        if (Object.keys(studentGroups).includes(major)) {
+          const group = studentGroups[major];
+          responseText = `List: ${group.map((student) => student.firstname).join(', ')}`;
+        }
+        response.status(200).send(responseText);
+      })
+      .catch((err) => {
+        response
+          .status(500)
+          .send(err instanceof Error ? err.message : err.toString());
+      });
   }
 }
+
+export default StudentsController;
+module.exports = StudentsController;
