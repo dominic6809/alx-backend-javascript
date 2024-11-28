2bc6e545f6e460ec2e02275a96b2cb7c2a7b28d4
Dominic Muuo <124082859+dominic6809@users.noreply.github.com>
2024-11-28 10:40:26 +0300

    mocha test

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,9 +1,3 @@
-/**
- * Function that rounds two numbers and returns their sum
- * @param {number} a - First number
- * @param {number} b - Second number
- * @returns {number} - The sum of rounded a and b
- */
 function calculateNumber(a, b) {
   return Math.round(a) + Math.round(b);
 }
