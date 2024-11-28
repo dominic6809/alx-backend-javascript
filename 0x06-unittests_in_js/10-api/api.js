662c22b7ff3e2ac9eba69bfe0212ddca014db7ae
dominic6809 <muuomusyoki018@gmail.com>
2024-11-28 08:51:13 +0000

    Deep equality & Post integration testing

---
+++ 0x06-unittests_in_js/10-api/api.js
@@ -0,0 +1,40 @@
+const express = require('express');
+
+const app = express();
+const PORT = 7865;
+
+// Middleware to parse JSON body
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+// Route for /cart/:id with validation
+app.get('/cart/:id(\\d+)', (req, res) => {
+  const id = req.params.id;
+
+  res.send(`Payment methods for cart ${id}`);
+});
+
+// Route for /available_payments
+app.get('/available_payments', (_req, res) => {
+  res.json({ payment_methods: { credit_cards: true, paypal: false } });
+});
+
+// Route for /login
+app.post('/login', (req, res) => {
+  let username = '';
+
+  if (req.body) {
+    username = req.body.userName;
+  }
+
+  res.send(`Welcome ${username}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`API available on localhost port ${PORT}`);
+});
+
+module.exports = app;
